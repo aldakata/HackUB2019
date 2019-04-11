de2139a85a45e5440f65f3aab96106cc6c4aa60c
Eudaldz <eudald.elias@gmail.com>
2019-04-11 10:09:59 +0200

    hola

---
+++ Hackathon 2019/Assets/Scripts/InputController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum InputAction{
+	UP, DOWN, LEFT, RIGHT, A1, A2
+}
+
+public class InputController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Hackathon 2019/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum Direction {
+	UP, DOWN, LEFT, RIGHT, NULL
+}
+
+public class PlayerController : MonoBehaviour {
+
+	private Direction movingDir;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	
+	public void DoAction(InputAction ia){
+		switch(ia){
+		case InputAction.UP:
+			break;
+		case InputAction.DOWN:
+			break;
+		case InputAction.LEFT:
+			break;
+		case InputAction.RIGHT:
+			break;
+		case InputAction.A1:
+			break;
+		case InputAction.A2:
+			break;
+		
+		}
+	}
+}
