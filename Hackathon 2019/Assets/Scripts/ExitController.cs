b553b261cb07bb605a5ca75e736e331927dc37f9
daver35 <daviddelapaz5@gmail.com>
2019-04-11 14:36:55 +0200

    Added exit

---
+++ Hackathon 2019/Assets/Scripts/ExitController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExitController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	//sets isPressed to true when pressed
+	void OnTriggerEnter2D(Collider2D col) {
+		Debug.Log ("Next Level");
+		//SceneManager.LoadScene("name")
+	}
+}
